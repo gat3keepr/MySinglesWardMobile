9ab4941922a0179a74af63e3919c3e335adfe8bd
Porter Hoskins <gatekeeper@Porters-Mac-mini.local>
2012-04-25 17:33:24 -0600

    Designed login page

---
+++ MySinglesWard/MSWLoginTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MSWLoginTableViewController.h
+//  MySinglesWard
+//
+//  Created by Porter Hoskins on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSWLoginTableViewController : UITableViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *emailAddressField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@end
